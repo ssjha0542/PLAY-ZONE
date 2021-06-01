e07127640b05f8b2a13ed369e9267aabf05cd66d
Shashank <ssjha0542@gmail.com>
2021-06-01 19:01:26 +0530

    added till the cross button and its functionality

--- script.js
+++ script.js
@@ -2,44 +2,50 @@ let ticTocToe=document.querySelector(".game_1");
 let snake=document.querySelector(".game_2");
 let flipFlop=document.querySelector(".game_3");
 
-
 ticTocToe.addEventListener("click",function(){
     let boxtoe=document.createElement("div");
-    boxtoe.setAttribute("class","container");
-    boxtoe.style.height= "calc( 100vh - ( 70px + 70px ) )";
-    boxtoe.style.width= "calc( 100vw - ( 100px + 100px ) )";
-    boxtoe.style.backgroundColor="grey";
-    boxtoe.style.position="absolute";
-    boxtoe.style.top="70px";
-    boxtoe.style.left="100px";
-    boxtoe.style.borderRadius="1rem";
-    console.log(boxtoe.style);
+    boxtoe.setAttribute("class","box_container");
+    
+    //console.log(boxtoe);
     document.body.appendChild(boxtoe);
+    handleBoxContainer();    
 });
+function handleBoxContainer(){
+    let selectBox=document.querySelector(".box_container");
+    let crossArea=document.createElement("div");
+    crossArea.setAttribute("class","cross");
+    crossArea.style.height="40px";
+    crossArea.innerHTML=`<i class="fas fa-times"ICON></i></div>`;
+    selectBox.appendChild(crossArea);
+    selectBox=document.querySelector(".box_container");
+    let crossBtn=document.querySelector(".fa-times");
+
+    crossBtn.addEventListener("click",function(){
+        selectBox=document.querySelector(".box_container");
+    console.log(selectBox);
+    while(selectBox.firstChild){
+        selectBox.removeChild(selectBox.firstChild);
+    }
+    selectBox.remove("box_container");
+    console.log(selectBox);
+    })
+}
+
+
+
+
 snake.addEventListener("click",function(){
     let boxtoe=document.createElement("div");
-    boxtoe.setAttribute("class","container");
-    boxtoe.style.height= "calc( 100vh - ( 70px + 70px ) )";
-    boxtoe.style.width= "calc( 100vw - ( 100px + 100px ) )";
-    boxtoe.style.backgroundColor="grey";
-    boxtoe.style.position="absolute";
-    boxtoe.style.top="70px";
-    boxtoe.style.left="100px";
-    boxtoe.style.borderRadius="1rem";
+    boxtoe.setAttribute("class","box_container");
     
-    console.log(boxtoe.style);
     document.body.appendChild(boxtoe);
+    handleBoxContainer();
 });
 flipFlop.addEventListener("click",function(){
     let boxtoe=document.createElement("div");
-    boxtoe.setAttribute("class","container");
-    boxtoe.style.height= "calc( 100vh - ( 70px + 70px ) )";
-    boxtoe.style.width= "calc( 100vw - ( 100px + 100px ) )";
-    boxtoe.style.backgroundColor="grey";
-    boxtoe.style.position="absolute";
-    boxtoe.style.top="70px";
-    boxtoe.style.left="100px";
-    boxtoe.style.borderRadius="1rem";
+    boxtoe.setAttribute("class","box_container");
+    
     console.log(boxtoe.style);
     document.body.appendChild(boxtoe);
+    handleBoxContainer();
 });
